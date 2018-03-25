8e68a16f265d0aa9067ab51140861d9c6cb17d6d
allen.mh <allen.mh@alibaba-inc.com>
2018-03-25 21:14:27 +0800

    add parameters when initial xml string

--- internal/config/init_config_xml.go
+++ internal/config/init_config_xml.go
@@ -1,38 +1,38 @@
 package config
 
-func InitConfigString() string {
+func InitConfigString(data map[string]string) string {
 	xml := `
 		<?xml version="1.0" encoding="utf-8" ?>
 		<logs>
 			<info >
 				<!-- info内容，先缓存到一定10条，再一次性输出 -->
 				<buffer size="1">
-					<rotate filename="%Y%m%d.%i.access.log" dir="-" size="20M" />
+					<rotate filename="%Y%m%d.%i.access.log" dir="` + data["path"] + `" size="20M" />
 				</buffer>
 			</info>
 
 			<debug>
-				<rotate  filename="%Y%m%d.%i.debug.log" dir="-" size="20M" />
+				<rotate  filename="%Y%m%d.%i.debug.log" dir="` + data["path"] + `" size="20M" />
 			</debug>
 
 			<trace>
-				<rotate  filename="%Y%m%d.%i.trace.log" dir="-" size="20M" />
+				<rotate  filename="%Y%m%d.%i.trace.log" dir="` + data["path"] + `" size="20M" />
 			</trace>
 
 			<warn>
 				<buffer size="5">
-					<rotate  filename="%Y%m%d.%i.warn.log" dir="-" size="20M" />
+					<rotate  filename="%Y%m%d.%i.warn.log" dir="` + data["path"] + `" size="20M" />
 				</buffer>
 			</warn>
 
 			<error>
+				<rotate filename="%Y%m%d.%i.error.log" dir="` + data["path"] + `" size="20M" />
 				<console output="stderr" foreground="red" background="blue" />
-				<rotate filename="%Y%m%d.%i.error.log" dir="-" size="20M" />
 			</error>
 
 			<critical>
+				<rotate filename="%Y%m%d.%i.critical.log" dir="` + data["path"] + `" size="20M" />
 				<console output="stderr" foreground="red" background="blue" />
-				<rotate filename="%Y%m%d.%i.critical.log" dir="-" size="20M" />
 			</critical>
 		</logs>
 `
