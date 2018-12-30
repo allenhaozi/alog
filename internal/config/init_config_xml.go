e5a4c7ae34b65f3989a93cb95ad306918fdcb352
allenhaozi <allenhaozi@gmail.com>
2018-12-30 17:02:27 +0800

    add buffer size in info log

--- internal/config/init_config_xml.go
+++ internal/config/init_config_xml.go
@@ -1,27 +1,28 @@
 package config
 
-var (
-	dateFormat = "%Y%m%d%H.%i"
-	kindList   map[string]string
+const (
+	info = "info"
 )
 
-func init() {
-	kindList = map[string]string{
-		"info":     "access.log",
+var (
+	dateFormat = "%Y%m%d%H.%i"
+	kindList   = map[string]string{
+		info:       "access.log",
 		"debug":    "debug.log",
 		"trace":    "trace.log",
 		"warn":     "warn.log",
 		"error":    "error.log",
 		"critical": "critical.log",
 	}
-}
+	defBufCnt = "10"
+)
 
 func InitConfigString(data map[string]string) string {
 
 	str := `<?xml version="1.0" encoding="utf-8" ?>
 			<logs>`
 	for k, v := range kindList {
-		tmp := buildXmlConfig(k, v, data["path"], data["size"])
+		tmp := buildXmlConfig(k, v, data["path"], data["size"], data["buf_cnt"])
 		str = str + `
 			  ` + tmp
 	}
@@ -32,46 +33,21 @@ func InitConfigString(data map[string]string) string {
 	return str
 }
 
-func buildXmlConfig(key, value, path, size string) string {
-	str := "<" + key + `>
+func buildXmlConfig(key, value, path, size, cnt string) string {
+	var str string
+	if key == info {
+		if len(cnt) <= 0 {
+			cnt = defBufCnt
+		}
+		str = "<" + key + `>
+				<buffer size="cnt">
+				<rotate filename="` + dateFormat + `.` + value + `" dir="` + path + `" size="` + size + `" />
+				</buffer>
+		    </` + key + ">"
+	} else {
+		str = "<" + key + `>
 				<rotate filename="` + dateFormat + `.` + value + `" dir="` + path + `" size="` + size + `" />
 		    </` + key + ">"
-
+	}
 	return str
-}
-
-func xmlbak(data map[string]string) string {
-	xml := `
-		<?xml version="1.0" encoding="utf-8" ?>
-		<logs>
-			<info >
-				<rotate filename="` + dateFormat + `.access.log" dir="` + data["path"] + `" size="` + data["size"] + `" />
-			</info>
-
-			<debug>
-				<rotate  filename="%Y%m%d.%H.%i.debug.log" dir="` + data["path"] + `" size="` + data["size"] + `" />
-			</debug>
-
-			<trace>
-				<rotate  filename="%Y%m%d.%i.trace.log" dir="` + data["path"] + `" size="` + data["size"] + `" />
-			</trace>
-
-			<warn>
-				<buffer size="5">
-					<rotate  filename="%Y%m%d.%i.warn.log" dir="` + data["path"] + `" size="` + data["size"] + `" />
-				</buffer>
-			</warn>
-
-			<error>
-				<rotate filename="%Y%m%d.%H.%i.error.log" dir="` + data["path"] + `" size="` + data["size"] + `" />
-				<console output="stderr" foreground="red" background="blue" />
-			</error>
-
-			<critical>
-				<rotate filename="%Y%m%d.%i.critical.log" dir="` + data["path"] + `" size="` + data["size"] + `" />
-				<console output="stderr" foreground="red" background="blue" />
-			</critical>
-		</logs>
-		`
-	return xml
 }
