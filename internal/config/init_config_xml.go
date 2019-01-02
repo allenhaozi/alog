a7bc31bcebbccbc36ffb339e835a60f9d56e929c
allenhaozi <allenhaozi@gmail.com>
2019-01-02 11:01:36 +0800

    add cnt

--- internal/config/init_config_xml.go
+++ internal/config/init_config_xml.go
@@ -5,8 +5,9 @@ const (
 )
 
 var (
-	dateFormat = "%Y%m%d%H.%i"
-	kindList   = map[string]string{
+	dateFormatYmdHi = "%Y%m%d%H.%i"
+	dateFormatYmdi  = "%Y%m%d.%i"
+	kindList        = map[string]string{
 		info:       "access.log",
 		"debug":    "debug.log",
 		"trace":    "trace.log",
@@ -36,17 +37,18 @@ func InitConfigString(data map[string]string) string {
 func buildXmlConfig(key, value, path, size, cnt string) string {
 	var str string
 	if key == info {
-		if len(cnt) <= 0 {
+		i, err := strconv.Atoi(cnt)
+		if i <= 0 || err != nil {
 			cnt = defBufCnt
 		}
 		str = "<" + key + `>
-				<buffer size="cnt">
-				<rotate filename="` + dateFormat + `.` + value + `" dir="` + path + `" size="` + size + `" />
+				<buffer size="` + cnt + `">
+				<rotate filename="` + dateFormatYmdHi + `.` + value + `" dir="` + path + `" size="` + size + `" />
 				</buffer>
 		    </` + key + ">"
 	} else {
 		str = "<" + key + `>
-				<rotate filename="` + dateFormat + `.` + value + `" dir="` + path + `" size="` + size + `" />
+				<rotate filename="` + dateFormatYmdi + `.` + value + `" dir="` + path + `" size="` + size + `" />
 		    </` + key + ">"
 	}
 	return str
